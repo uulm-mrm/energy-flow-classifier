86de557f00da3e2695c0f06169c3ecd847d00a31
Vladimir Lunic <vladimir.lunic@uni-ulm.de>
2025-10-27 13:26:19 +0100

    First experiment

--- experiments/mnist_clf.py
+++ experiments/mnist_clf.py
@@ -1,21 +1,20 @@
-from pprint import pprint
-
+import math
 from tqdm import tqdm
 
+import matplotlib.pyplot as plt
+
 import torch
+from torch.distributions import Independent, Normal
 
 from flow_matching.flow_matching import (
-    Path,
+    MultiPath,
     ODEProcess,
     MidpointIntegrator,
-    GoldenSectionSeeker,
+    NaiveMidpoints,
 )
-from flow_matching.flow_matching.scheduler import OTScheduler
-from flow_matching.flow_matching.distributions import GaussianMixture
-from flow_matching.flow_matching.utils import push_forward_all
-from flow_matching.modules.utils import EMA
+from flow_matching.flow_matching.scheduler import CosineMultiScheduler
 
-from unet import TimeCondUnet
+from models.unet import TimeCondUnet
 from utils import closest_anchor, anchors_to_class
 
 from data.mnist import get_mnist, MNISTSampler
@@ -27,11 +26,14 @@ def main():
     # consts
     device = "cuda:0"
 
-    classes = (0, 1, 2)
-    anchor_times = (0.0, 0.33, 0.66, 1.0)  # manually or with linspace
-    time_class_map = dict(zip(anchor_times, (-1, *classes)))
+    classes = (0,)
+    anchor_times = torch.tensor([0.0, 1.0], dtype=torch.float32, device=device)
+    path_width = 1.0  # should be dt between classes
 
-    batch_size = 1024
+    # might mess up classification
+    time_class_map = dict(zip(anchor_times.cpu().numpy(), (-1, *classes)))
+
+    batch_size = 512
 
     t_dims = 64
     lr = 1e-3
@@ -42,18 +44,11 @@ def main():
         mnist, classes=classes, batch_size=batch_size, device=device
     )
 
-    # mnist image dims are 1, 28, 28
-    x0_sampler = GaussianMixture(
-        n=8, shape=(1, 28, 28), sigma=0.5, r=1.0, device=device
-    )
-
     # model stuff
     unet = TimeCondUnet(in_c=1, filters=32, heads=4, t_dims=t_dims).to(device)
-    ema = EMA(unet, rate=0.999).to(device)
-    path = Path(OTScheduler())
+    path = MultiPath(CosineMultiScheduler(k=path_width))
 
-    optim = torch.optim.AdamW(ema.model.parameters(), lr=lr, weight_decay=1e-4)
-    sched = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=0.95)
+    optim = torch.optim.AdamW(unet.parameters(), lr=lr)
 
     # training
     for _ in (pbar := tqdm(range(epochs))):
@@ -62,41 +57,112 @@ def main():
         for x in x_sampler:
             optim.zero_grad()
 
-            x0 = x0_sampler.sample(x[0].shape[0])
-            anchor_x = (x0, *x)
+            x0 = torch.randn_like(x[0])
+            t = torch.rand((x0.shape[0]), dtype=torch.float32, device=device)
 
-            loss = push_forward_all(
-                anc_x=anchor_x, anc_t=anchor_times, path=path, vf=ema.model
-            )
+            path_sample = path.sample(torch.stack([x0, *x], dim=0), anchor_times, t)
+
+            dxt_hat = unet.forward(path_sample.xt, path_sample.t)
+
+            loss = (dxt_hat - path_sample.dxt).square().mean()
 
             loss.backward()
             optim.step()
 
-            ema.update_ema_t()
-
             epoch_loss = epoch_loss + loss
 
-        sched.step()
         pbar.set_description(f"Loss: {(epoch_loss / x_sampler.batches):.3f}")
 
-    ema.to_model()
-    unet = ema.model.eval()
+    unet = unet.eval()
 
-    # classification
+    # probability stuff
     integrator = ODEProcess(unet, MidpointIntegrator())
-    seeker = GoldenSectionSeeker(max_evals=10)
-    ode_steps = 10
-    log_p0 = x0_sampler.log_likelihood
+    seeker = NaiveMidpoints(max_evals=30, iters=3)
+    ode_steps = 20
+    log_p0 = log_p0 = Independent(
+        Normal(
+            torch.zeros((1, 28, 28), device=device),
+            torch.ones((1, 28, 28), device=device),
+        ),
+        reinterpreted_batch_ndims=3,
+    ).log_prob
 
-    # classify digits 0
-    x1 = x_sampler.data[x_sampler.indices[0][:1000]]
-
-    min_t, _ = integrator.classify(
-        seeker, x1.to(device), log_p0, steps=ode_steps, est_steps=1, eps=1e-4
+    # plot path
+    t_traj, x_traj = integrator.sample(
+        x_init=torch.randn((1, 1, 28, 28), dtype=torch.float32, device=device),
+        ints=torch.tensor(
+            [[anchor_times[0].item(), anchor_times[-1].item()]],
+            dtype=torch.float32,
+            device=device,
+        ),
+        steps=ode_steps,
     )
 
+    # plot path
+    sols = x_traj.detach().cpu().numpy()
+
+    ax_cols = math.ceil(sols.shape[0] ** 0.5)
+    ax_rows = math.ceil(sols.shape[0] / ax_cols)
+    fig, axs = plt.subplots(ax_rows, ax_cols, figsize=(ax_cols * 4, ax_rows * 4))
+
+    # yes you can flatten axes they are a np.array
+    axs = axs.flatten()  # type: ignore
+    for i, (time, sol) in enumerate(zip(t_traj, sols)):
+        sol = sol.reshape(28, 28)
+        time = time.reshape(1).item()
+
+        axs[i].imshow(sol, cmap="gray")
+        axs[i].set_title(f"Time: {time:.3f}")
+        axs[i].set_aspect("equal")
+
+    for i in range(sols.shape[0], len(axs)):
+        fig.delaxes(axs[i])
+
+    plt.tight_layout()
+    plt.show()
+
+    # plot prob
+    for c in classes:
+        indices = x_sampler.indices[classes[c]][:50]
+        batch = len(indices)
+        t_steps = 20
+
+        t = torch.linspace(0.0, anchor_times[-1], steps=t_steps, device=device).repeat(
+            batch
+        )
+        intervals = torch.zeros(
+            (t_steps * batch, 2), dtype=torch.float32, device=device
+        )
+        intervals[:, 0] = t
+
+        x = x_sampler.data[indices].repeat_interleave(t_steps, dim=0).to(device)
+        _, probs = integrator.compute_likelihood(
+            x, intervals, log_p0, steps=ode_steps, est_steps=5
+        )
+
+        plt.gca().invert_xaxis()
+        for prob, interval in zip(probs.chunk(batch), intervals.chunk(batch)):
+            plt.plot(interval[:, 0].cpu().numpy(), prob.cpu().numpy())
+        plt.show()
+
+    # classify digits
+    indices = torch.cat([x_sampler.indices[c][:5] for c in classes], dim=0)
+    x = x_sampler.data[indices]
+    print(x_sampler.labels[indices])
+
+    min_t, _ = integrator.classify(
+        seeker,
+        x.to(device),
+        log_p0,
+        interval=(anchor_times[0].item(), anchor_times[-1].item()),
+        steps=ode_steps,
+        est_steps=1,
+        eps=1e-8,
+    )
+    print(min_t)
+
     preds = anchors_to_class(closest_anchor(min_t, anchor_times), time_class_map)  # type: ignore
-    print(sum(preds == 0))
+    print(preds)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,15 +2,11 @@ import torch
 from torch import Tensor
 
 
-def closest_anchor(time_predictions: Tensor, anchor_times: tuple[float, ...]) -> Tensor:
+def closest_anchor(time_predictions: Tensor, anchor_times: Tensor) -> Tensor:
     """Maps time predictions to their closest anchor times"""
 
-    anc_times = torch.tensor(
-        anchor_times, dtype=time_predictions.dtype, device=time_predictions.device
-    )
-
-    return anc_times[
-        torch.argmin((time_predictions[:, None] - anc_times[None]).abs(), dim=1)
+    return anchor_times[
+        torch.argmin((time_predictions[:, None] - anchor_times[None]).abs(), dim=1)
     ]
 
 
@@ -27,12 +23,12 @@ def anchors_to_class(
 
 def main():
     preds = torch.tensor([0.14, 0.45, 0.89, 0.12])
-    times = (0.0, 0.33, 0.66, 1.0)
+    times = torch.tensor([0.0, 0.33, 0.66, 1.0])
 
     res = closest_anchor(preds, times)
     print(res)
 
-    pred_cls = anchors_to_class(res, dict(zip(times, (-1, 0, 1, 2))))
+    pred_cls = anchors_to_class(res, dict(zip(times.numpy(), (-1, 0, 1, 2))))
     print(pred_cls)
 
 
