db4558cae345955be1ca0d2bfba2cfb77a64994d
Vladimir Lunic <vladimir.lunic@uni-ulm.de>
2025-11-10 09:29:15 +0100

    Train on 5 classes well

--- experiments/mnist_gen.py
+++ experiments/mnist_gen.py
@@ -1,68 +1,49 @@
+from matplotlib import pyplot as plt
+import torch
 from tqdm import tqdm
 
-import matplotlib.pyplot as plt
-
-import torch
-
-from flow_matching.flow_matching import (
-    AffineMultiPath,
-    AffinePath,
-    ODEProcess,
-    RungeKuttaIntegrator,
-)
-from flow_matching.flow_matching.distributions import MultiIndependentNormal
+from data.mnist import MNISTSampler, get_mnist
+from flow_matching.flow_matching.integrator import RungeKuttaIntegrator
 from flow_matching.flow_matching.integrator_utils import RK4_TABLEAU
+from flow_matching.flow_matching.path import AffinePath
+from flow_matching.flow_matching.process import ODEProcess
 from flow_matching.flow_matching.scheduler import CosineScheduler
-from flow_matching.modules.utils import EMA
-
+from flow_matching.modules.utils.ema import EMA
 from models.unet import UNet
-
-from data.mnist import get_mnist, MNISTSampler
 
 
 def main():
     torch.manual_seed(42)
 
-    # consts
     device = "cuda:0"
     mnist_shape = (1, 32, 32)
-    classes = tuple(range(5))
+    classes = (0,)
 
-    num_classes = len(classes)
-    r = 5.0
-    sigma = 1.0
-
-    batch_size = 512
+    batch_size = 1024
     t_dims = 256
     lr = 1e-3
     epochs = 1024
 
-    # dataset
     mnist = get_mnist("train")
     mnist_sampler = MNISTSampler(
         mnist, classes=classes, batch_size=batch_size, device=device, skip_last=True
     )
 
-    multi_normal = MultiIndependentNormal(
-        c=num_classes, shape=mnist_shape, r=r, sigma=sigma, device=device
-    )
-
-    # model stuff
-    net = UNet(in_c=1, out_c=1, features=[64, 128, 256], t_dims=t_dims).to(device)
+    net = UNet(in_c=1, out_c=1, features=[32, 64, 128], t_dims=t_dims).to(device)
     ema = EMA(net, rate=0.999)
-    path = AffineMultiPath(AffinePath(CosineScheduler()), num_classes)
+
+    path = AffinePath(CosineScheduler())
 
     optim = torch.optim.AdamW(net.parameters(), lr=lr)
 
-    # training
     for _ in (pbar := tqdm(range(epochs))):
         epoch_loss = 0.0
 
         for x in mnist_sampler:
             optim.zero_grad()
 
-            x0 = multi_normal.sample(x[0].shape[0])
-            x1 = torch.stack(x, dim=0)
+            x0 = torch.randn_like(x[0])
+            x1 = x[0]
             t = torch.rand((x[0].shape[0],), dtype=torch.float32, device=device)
 
             path_sample = path.sample(x0, x1, t)
@@ -86,24 +67,14 @@ def main():
     ode_steps = 100
 
     _, x_traj = proc.sample(
-        multi_normal.means,
+        torch.zeros((1, *mnist_shape), dtype=torch.float32, device=device),
         ints=torch.tensor([[0.0, 1.0]], dtype=torch.float32, device=device),
         steps=ode_steps,
     )
 
     sols = x_traj[-1].detach().cpu().numpy()
-
-    for i, sol in enumerate(sols):
-        plt.imshow(sol[0], "gray")
-        plt.savefig(f"./plots/sol{i}.jpg")
-
-    # classify a couple of samples
-    # indices = torch.cat(
-    #     [mnist_sampler.indices[0][:1], mnist_sampler.indices[1][:1]], dim=0
-    # )
-    # x = mnist_sampler.data[indices]
-
-    # intervals = torch.tensor([[1.0, 0.0]])
+    plt.imshow(sols[0][0], "gray")
+    plt.show()
 
 
 if __name__ == "__main__":
