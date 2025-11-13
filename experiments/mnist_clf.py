bff24a2cb6a233b304a14c4c828d4bf36beee8a3
Vladimir Lunic <vladimir.lunic@uni-ulm.de>
2025-11-13 09:46:45 +0100

    Working n-class

--- experiments/mnist_clf.py
+++ experiments/mnist_clf.py
@@ -1,3 +1,5 @@
+import math
+
 from tqdm import tqdm
 
 import matplotlib.pyplot as plt
@@ -20,22 +22,25 @@ from models.unet import UNet
 from data.mnist import get_mnist, MNISTSampler
 
 
-def main():
+def train():
     torch.manual_seed(42)
 
     # consts
     device = "cuda:0"
     mnist_shape = (1, 32, 32)
+    mnist_dims = math.prod(mnist_shape)
     classes = tuple(range(3))
 
     num_classes = len(classes)
-    r = 5.0
+
     sigma = 1.0
+    k = 3.0
+    r = k * sigma * mnist_dims**0.5
 
     batch_size = 512
     t_dims = 256
     lr = 1e-3
-    epochs = 1024
+    epochs = 5000
 
     # dataset
     mnist = get_mnist("train")
@@ -48,7 +53,7 @@ def main():
     )
 
     # model stuff
-    net = UNet(in_c=1, out_c=1, features=[32, 64, 128], t_dims=t_dims).to(device)
+    net = UNet(in_c=1, out_c=1, features=[64, 128, 256], t_dims=t_dims).to(device)
     ema = EMA(net, rate=0.999)
     path = AffineMultiPath(AffinePath(CosineScheduler()), num_classes)
 
@@ -72,6 +77,8 @@ def main():
             loss = (dxt_hat - path_sample.dxt).square().mean()
 
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=10.0)
+
             optim.step()
 
             ema.update_ema_t()
@@ -85,21 +92,69 @@ def main():
 
     torch.save(net.state_dict(), f"trained/mnist_{num_classes}.pt")
 
+
+def test():
+    torch.manual_seed(42)
+
+    # consts
+    device = "cuda:0"
+    mnist_shape = (1, 32, 32)
+    mnist_dims = math.prod(mnist_shape)
+    classes = tuple(range(3))
+
+    num_classes = len(classes)
+
+    sigma = 1.0
+    k = 3.0
+    r = k * sigma * mnist_dims**0.5
+
+    batch_size = 512
+    t_dims = 256
+
+    # dataset
+    mnist = get_mnist("train")
+    mnist_sampler = MNISTSampler(
+        mnist, classes=classes, batch_size=batch_size, device=device, skip_last=True
+    )
+
+    multi_normal = MultiIndependentNormal(
+        c=num_classes, shape=mnist_shape, r=r, sigma=sigma, device=device
+    )
+
+    net = UNet(in_c=1, out_c=1, features=[64, 128, 256], t_dims=t_dims).to(device)
+    net.load_state_dict(torch.load(f"trained/mnist_{num_classes}.pt"))
+    net = net.eval()
+
+    # process to integrate
     proc = ODEProcess(net, RungeKuttaIntegrator(RK4_TABLEAU, device=device))
     ode_steps = 100
 
-    _, x_traj = proc.sample(
-        multi_normal.means,
-        ints=torch.tensor([[1.0, 0.0]], dtype=torch.float32, device=device),
-        steps=ode_steps,
+    # generate
+    # x_noise = multi_normal.means
+    x_noise = multi_normal.sample(3).reshape((-1, *mnist_shape))
+    intervals = torch.tensor([[1.0, 0.0]], dtype=torch.float32, device=device).expand(
+        x_noise.shape[0], 2
     )
-
+    _, x_traj = proc.sample(x_noise, ints=intervals, steps=ode_steps)
     sols = x_traj[-1].detach().cpu().numpy()
 
-    for i, sol in enumerate(sols):
+    for sol in sols:
         plt.imshow(sol[0], "gray")
-        plt.savefig(f"./plots/sol{i}.jpg")
+        plt.show()
+
+    # predict
+    x_pred = mnist_sampler.data[mnist_sampler.indices[0]].to(device)
+    intervals = torch.tensor([[0.0, 1.0]], dtype=torch.float32, device=device).expand(
+        x_pred.shape[0], 2
+    )
+
+    _, x_traj = proc.sample(x_pred, intervals, steps=ode_steps)
+    sols = x_traj[-1]
+    probs = multi_normal.log_likelihood(sols)
+    print(x_pred.shape[0])
+    print(torch.sum(probs.argmax(dim=1) == 0))
 
 
 if __name__ == "__main__":
-    main()
+    # train()
+    test()
